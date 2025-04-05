d7fef8ef9452f7f2cece0100aa8c5381117d83af
ManuelWestermeier <westermeier111@gmail.com>
2025-04-05 22:18:52 +0200

    bcRef.current

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export function useShared(key = "", defaultValue) {
     }, [key]);
 
     useEffect(() => {
-        bc.postMessage({
+        bcRef.current.postMessage({
             type: "get",
         });
     }, []);
